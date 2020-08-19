d1619c3e363e03a0bce542bc3ccb2f8e53b08103
Sophia <SSPuliasis@dundee.ac.uk>
2020-08-19 16:26:05 +0100

    residue filter script started and summary table set as function

--- residue_filter.py
+++ residue_filter.py
@@ -1,13 +1,18 @@
 # Filter peptides in the processed RPG output format (CSV) for peptides containing
 # certain amino acids
 
-#start by using cysteine as an example
-
-#packages used
 import pandas as pd
 
 
-#import the rpg results/ peptide table
+def filter_for_residue(residue, input_file, output_file):
+    unfiltered_file = pd.read_csv(input_file)
+    filtered_file = pd.DataFrame()
+    for index, row in unfiltered_file.iterrows():
+        if residue in row['sequence']:
+            filtered_file = filtered_file.append(row)
+    filtered_file.to_csv(output_file)
 
-# read through and filter similarly to junction spanning
-# write positive outcomes to a new file
+
+# EXAMPLE
+# input and output file names need to have csv extension and residue should be capitalised
+filter_for_residue('C', 'at1g6600_at1g6610_fwd_rpg_filtered.csv', 'test1.csv')
