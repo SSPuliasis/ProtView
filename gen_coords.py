fe9fdd5dd7c2fc51644a8f32ec2d6e20ec74398e
Sophia <SSPuliasis@dundee.ac.uk>
2021-04-29 15:47:34 +0100

    gen_coords.py script tidied up

--- gen_coords.py
+++ gen_coords.py
@@ -21,43 +21,31 @@ def calculate_gen_coords(peptides_file, cds_files):
     strand_peptides = input_test[input_test['parent'].isin(isoformlist)]
 
     # make an alphabetical list of the enzymes from the rpg output
-    enzyme_set = set(strand_peptides['enzyme'].tolist())
-    enzyme_list = []
-    for protease in enzyme_set:
-        enzyme_list.append(protease)
-    enzyme_list = sorted(enzyme_list)
+    enzyme_list = sorted(set(strand_peptides['enzyme'].tolist()))
 
     # define this df outside the loop
     overall_df = pd.DataFrame()
 
     # filter the input dataframe by enzyme
     for protease in enzyme_list:
-        peptides_by_enzyme = strand_peptides[strand_peptides['enzyme'] == protease]  # change thie to peptides_by_enzyme if it works
+        peptides_by_enzyme = strand_peptides[strand_peptides['enzyme'] == protease]
 
         merged_prot = {}
-
-        for pos, rpg_parent in zip(peptides_by_enzyme.peptide_start, peptides_by_enzyme.parent):
-            for cds_start, cds_end, cds_id, cds_parent in zip(processed_cds.protein_start, processed_cds.protein_end,
-                                                              processed_cds.cds_id, processed_cds.Parent):
-                if rpg_parent.endswith(cds_parent):
-                    if pos in range(cds_start, cds_end + 1):
-                        merged_prot[cds_parent, pos] = {}
-                        merged_prot[cds_parent, pos] = cds_id
-                    else:
-                        continue
-
         merged_gen = {}
 
-        for dict_key, dict_cds_id in merged_prot.items():
-            for df_cds_id, first_start, cum_intron, cds_parent in zip(processed_cds.cds_id, processed_cds.cds_first_start,
-                                                                      processed_cds.cum_intron, processed_cds.Parent):
-                if dict_cds_id == df_cds_id:
-                    if cds_parent == dict_key[0]:
-                        pos_number = dict_key[1]
-                        gen_position = first_start + cum_intron + 3 * (pos_number - 1)
-                        merged_gen[cds_parent, gen_position] = {}
-                        merged_gen[cds_parent, gen_position] = df_cds_id
-                        # print('end positions', prot_position, 'corresponds to', gen_position, 'in', cdsid)
+        for index, cds_row in processed_cds.iterrows():
+            peptides_by_enzyme_parent = peptides_by_enzyme.loc[(peptides_by_enzyme['parent'].str.endswith(cds_row.Parent))
+                                                               & (peptides_by_enzyme.peptide_start <= cds_row.protein_end)
+                                                               & (peptides_by_enzyme.peptide_start >= cds_row.protein_start)]
+            for pos in peptides_by_enzyme_parent.peptide_start:
+                merged_prot[cds_row.Parent, pos] = cds_row.cds_id
+
+            for dict_key, dict_cds_id in merged_prot.items():
+                if dict_cds_id == cds_row.cds_id and cds_row.Parent == dict_key[0]:
+                    pos_number = dict_key[1]
+                    gen_position = cds_row.cds_first_start + cds_row.cum_intron + 3 * (pos_number - 1)
+                    merged_gen[cds_row.Parent, gen_position] = cds_row.cds_id
+                    # print('end positions', prot_position, 'corresponds to', gen_position, 'in', cdsid)
 
         isoform_start_positions_df = pd.DataFrame(columns=['isoform', 'cds_id', 'pept_start_pos', 'gen_start_pos'])
         for protkeys, genkeys in zip(merged_prot.items(), merged_gen.items()):
@@ -69,30 +57,21 @@ def calculate_gen_coords(peptides_file, cds_files):
         ###################################
 
         merged_prot = {}
-
-        for pos, rpg_parent in zip(peptides_by_enzyme.cleavage_position, peptides_by_enzyme.parent):
-            for cds_start, cds_end, cds_id, cds_parent in zip(processed_cds.protein_start, processed_cds.protein_end,
-                                                              processed_cds.cds_id, processed_cds.Parent):
-                if rpg_parent.endswith(cds_parent):
-                    if pos in range(cds_start, cds_end + 1):
-                        # print('The position ', pos, ' is in the cds ', cds_id, 'with parent', rpg_parent)
-                        merged_prot[cds_parent, pos] = {}
-                        merged_prot[cds_parent, pos] = cds_id
-                    else:
-                        continue
-
         merged_gen = {}
 
-        for dict_key, dict_cds_id in merged_prot.items():
-            for df_cds_id, first_start, cum_intron, cds_parent in zip(processed_cds.cds_id, processed_cds.cds_first_start,
-                                                                      processed_cds.cum_intron, processed_cds.Parent):
-                if dict_cds_id == df_cds_id:
-                    if cds_parent == dict_key[0]:
-                        pos_number = dict_key[1]
-                        gen_position = first_start + cum_intron + 3 * (pos_number - 1) +2
-                        merged_gen[cds_parent, gen_position] = {}
-                        merged_gen[cds_parent, gen_position] = df_cds_id
-                        # print('start position', prot_position, 'corresponds to', gen_position, 'in', cdsid)
+        for index, cds_row in processed_cds.iterrows():
+            peptides_by_enzyme_parent = peptides_by_enzyme.loc[(peptides_by_enzyme['parent'].str.endswith(cds_row.Parent))
+                                                               & (peptides_by_enzyme.peptide_start <= cds_row.protein_end)
+                                                               & (peptides_by_enzyme.peptide_start >= cds_row.protein_start)]
+            for pos in peptides_by_enzyme_parent.cleavage_position:
+                merged_prot[cds_row.Parent, pos] = cds_row.cds_id
+
+            for dict_key, dict_cds_id in merged_prot.items():
+                if dict_cds_id == cds_row.cds_id and cds_row.Parent == dict_key[0]:
+                    pos_number = dict_key[1]
+                    gen_position = cds_row.cds_first_start + cds_row.cum_intron + 3 * (pos_number - 1) + 2
+                    merged_gen[cds_row.Parent, gen_position] = cds_row.cds_id
+                    # print('end positions', prot_position, 'corresponds to', gen_position, 'in', cdsid)
 
         isoform_end_positions_df = pd.DataFrame(columns=['isoform', 'cds_id', 'pept_end_pos', 'gen_end_pos'])
         for protkeys, genkeys in zip(merged_prot.items(), merged_gen.items()):
@@ -123,43 +102,31 @@ def calculate_gen_coords(peptides_file, cds_files):
     strand_peptides = input_test[input_test['parent'].isin(isoformlist)]
 
     # make an alphabetical list of the enzymes from the rpg output
-    enzyme_set = set(strand_peptides['enzyme'].tolist())
-    enzyme_list = []
-    for protease in enzyme_set:
-        enzyme_list.append(protease)
-    enzyme_list = sorted(enzyme_list)
+    enzyme_list = sorted(set(strand_peptides['enzyme'].tolist()))
 
     # define this df outside the loop
     overall_df = pd.DataFrame()
 
     # filter the input dataframe by enzyme
     for protease in enzyme_list:
-        peptides_by_enzyme = input_test[input_test['enzyme'] == protease]  # change thie to peptides_by_enzyme if it works
+        peptides_by_enzyme = input_test[input_test['enzyme'] == protease]
 
         merged_prot = {}
-
-        for pos, rpg_parent in zip(peptides_by_enzyme.cleavage_position, peptides_by_enzyme.parent):
-            for cds_start, cds_end, cds_id, cds_parent in zip(processed_cds.protein_start, processed_cds.protein_end,
-                                                              processed_cds.cds_id, processed_cds.Parent):
-                if rpg_parent.endswith(cds_parent):
-                    if pos in range(cds_end, cds_start + 1):
-                        merged_prot[cds_parent, pos] = {}
-                        merged_prot[cds_parent, pos] = cds_id
-                    else:
-                        continue
-
         merged_gen = {}
 
-        for dict_key, dict_cds_id in merged_prot.items():
-            for df_cds_id, first_start, cum_intron, cds_parent in zip(processed_cds.cds_id, processed_cds.cds_first_end,
-                                                                      processed_cds.cum_intron, processed_cds.Parent):
-                if dict_cds_id == df_cds_id:
-                    if cds_parent == dict_key[0]:
-                        pos_number = dict_key[1]
-                        gen_position = first_start - cum_intron - 3 * (pos_number - 1) - 2
-                        merged_gen[cds_parent, gen_position] = {}
-                        merged_gen[cds_parent, gen_position] = df_cds_id
-                        # print('start position', prot_position, 'corresponds to', gen_position, 'in', cdsid)
+        for index, cds_row in processed_cds.iterrows():
+            peptides_by_enzyme_parent = peptides_by_enzyme.loc[(peptides_by_enzyme['parent'].str.endswith(cds_row.Parent))
+                                                               & (peptides_by_enzyme.peptide_start >= cds_row.protein_end)
+                                                               & (peptides_by_enzyme.peptide_start <= cds_row.protein_start)]
+            for pos in peptides_by_enzyme_parent.cleavage_position:
+                merged_prot[cds_row.Parent, pos] = cds_row.cds_id
+
+            for dict_key, dict_cds_id in merged_prot.items():
+                if dict_cds_id == cds_row.cds_id and cds_row.Parent == dict_key[0]:
+                    pos_number = dict_key[1]
+                    gen_position = cds_row.cds_first_end - cds_row.cum_intron - 3 * (pos_number - 1) - 2
+                    merged_gen[cds_row.Parent, gen_position] = cds_row.cds_id
+                    # print('end positions', prot_position, 'corresponds to', gen_position, 'in', cdsid)
 
         isoform_start_positions_df = pd.DataFrame(columns=['isoform', 'cds_id', 'pept_start_pos', 'gen_start_pos'])
         for protkeys, genkeys in zip(merged_prot.items(), merged_gen.items()):
@@ -170,29 +137,21 @@ def calculate_gen_coords(peptides_file, cds_files):
 
         ##############################################
         merged_prot = {}
-
-        for pos, rpg_parent in zip(peptides_by_enzyme.peptide_start, peptides_by_enzyme.parent):
-            for cds_start, cds_end, cds_id, cds_parent in zip(processed_cds.protein_start, processed_cds.protein_end,
-                                                              processed_cds.cds_id, processed_cds.Parent):
-                if rpg_parent.endswith(cds_parent):
-                    if pos in range(cds_end, cds_start + 1):
-                        # print('The position ', pos, ' is in the cds ', cds_id, 'with parent', rpg_parent)
-                        merged_prot[cds_parent, pos] = {}
-                        merged_prot[cds_parent, pos] = cds_id
-                    else:
-                        continue
         merged_gen = {}
 
-        for dict_key, dict_cds_id in merged_prot.items():
-            for df_cds_id, first_start, cum_intron, cds_parent in zip(processed_cds.cds_id, processed_cds.cds_first_end,
-                                                                      processed_cds.cum_intron, processed_cds.Parent):
-                if dict_cds_id == df_cds_id:
-                    if cds_parent == dict_key[0]:
-                        pos_number = dict_key[1]
-                        gen_position = first_start - cum_intron - 3 * (pos_number - 1)
-                        merged_gen[cds_parent, gen_position] = {}
-                        merged_gen[cds_parent, gen_position] = df_cds_id
-                        # print('end positions', prot_position, 'corresponds to', gen_position, 'in', cdsid)
+        for index, cds_row in processed_cds.iterrows():
+            peptides_by_enzyme_parent = peptides_by_enzyme.loc[(peptides_by_enzyme['parent'].str.endswith(cds_row.Parent))
+                                                               & (peptides_by_enzyme.peptide_start >= cds_row.protein_end)
+                                                               & (peptides_by_enzyme.peptide_start <= cds_row.protein_start)]
+            for pos in peptides_by_enzyme_parent.peptide_start:
+                merged_prot[cds_row.Parent, pos] = cds_row.cds_id
+
+            for dict_key, dict_cds_id in merged_prot.items():
+                if dict_cds_id == cds_row.cds_id and cds_row.Parent == dict_key[0]:
+                    pos_number = dict_key[1]
+                    gen_position = cds_row.cds_first_end - cds_row.cum_intron - 3 * (pos_number - 1)
+                    merged_gen[cds_row.Parent, gen_position] = cds_row.cds_id
+                    # print('end positions', prot_position, 'corresponds to', gen_position, 'in', cdsid)
 
         isoform_end_positions_df = pd.DataFrame(columns=['isoform', 'cds_id', 'pept_end_pos', 'gen_end_pos'])
         for protkeys, genkeys in zip(merged_prot.items(), merged_gen.items()):
