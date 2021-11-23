5cfe5366de1734b6b14bdd702b229920eedd96dd
Sophia <SSPuliasis@dundee.ac.uk>
2021-11-23 10:27:17 +0000

    setup.py added for upload to PyPi

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ProtView',
+    version='0.0.1',
+    packages=['modules'],
+    url='https://github.com/SSPuliasis/ProtView',
+    license='GPL v3',
+    author='Sophia Puliasis',
+    author_email='SSPuliasis@dundee.ac.uk',
+    description='Digest scheme evaluation in proteomic & proteogenomic context',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'numpy==1.16.5',
+        'pandas==0.25.1',
+        'gffpandas==1.2.0',
+        'BioPython==1.76',
+        'rpg==1.1.0'
+    ],
+    python_requires='==3.7',
+    setup_requires=['wheel']
+)
